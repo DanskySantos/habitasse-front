3c35bdbd84f451b9f438076f5e5d2e01aa42e767
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-02-23 14:35:24 -0300

    I added formsModule and toastmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
+import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+ 
 
 @NgModule({
     imports: [
         AppLayoutModule,
         AppRoutingModule,
+        FormsModule,
+        ToastModule
     ],
     declarations: [
         AppComponent,
+ 
     ],
     providers: [
         {provide: LocationStrategy, useClass: HashLocationStrategy}
