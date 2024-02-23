c912a11898a753cf3aad209329087fd2ab3e95c3
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-02-23 15:03:27 -0300

    creation environments.ts

---
+++ src/environments/environments.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8080',
+    clientId: 'userRegulacao',
+    clientSecret: '@2023%',
+    obterTokenUrl: '/seguranca/oauth/token'
+  
+    //tokenWhitelistedDomains: [/localhost:8080/ ],
+    //tokenBlacklistedRoutes: [/\/oauth\/oauth\/token/]
+  };
