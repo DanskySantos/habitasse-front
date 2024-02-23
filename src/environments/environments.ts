ffcbf21e211d7093d202a0081d2a00b39982c103
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-02-23 17:55:05 -0300

    change in environments.ts, changed clientId, apiUrl and getTokenUrl

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080',
-    clientId: 'userRegulacao',
+    clientId: 'user',
     clientSecret: '@2023%',
-    obterTokenUrl: '/seguranca/oauth/token'
+    obterTokenUrl: '/api/v1/auth/authenticate'
   
     //tokenWhitelistedDomains: [/localhost:8080/ ],
     //tokenBlacklistedRoutes: [/\/oauth\/oauth\/token/]
