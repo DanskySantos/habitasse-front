2a5248acfe049ed7a9a99d40e41a41ba57ee1cc3
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-02-23 20:08:45 -0300

    I removed getTokenUrl

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:8080/api/v1/auth/authenticate',
     clientId: 'user',
     clientSecret: '@2023%',
-    obterTokenUrl: '/api/v1/auth/authenticate'
-  
     //tokenWhitelistedDomains: [/localhost:8080/ ],
     //tokenBlacklistedRoutes: [/\/oauth\/oauth\/token/]
   };
