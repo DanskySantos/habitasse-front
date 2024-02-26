1bfb75afec231a4f90f7162578f0e2fea7dcc153
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-02-26 11:43:51 -0300

    Removal of production: true from environments.ts and also removal of the environments.prod.ts and environments.dev.ts pages

--- src/environments/environments.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8080',
-    clientId: 'userRegulacao',
-    clientSecret: '@2023%',
-    obterTokenUrl: '/seguranca/oauth/token'
-};
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
     apiUrl: 'http://localhost:8080/api/v1/auth/authenticate',
     clientId: 'user',
     clientSecret: '@2023%',
