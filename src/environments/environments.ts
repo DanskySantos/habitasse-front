d8747b0bb0774afe7789b61cae13f5fef7efc347
danSky <dansky123@hotmail.com>
2024-03-23 13:50:01 -0300

    changing api

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://habitasse-e15112ee0785.herokuapp.com/api/v1/',
+    apiUrl: 'https://habitasse-back-c34b7e694c02.herokuapp.com/api/v1/',
     clientId: 'qwsdfgsdfgsdetersadf',
     clientSecret: '@20242opasijopdjoipfasdjpio3%',
 }
