7c19853e3351c46993c6c19546fc6a47c8980f68
Kenedy Gomes Camargo <kenedy@digitalizaai.com.br>
2024-04-03 20:23:34 -0300

    Role change for shared.ts

--- src/app/demo/components/home/all-demands/create-update-offer.modal/create-update-offer-modal.component.ts
+++ src/app/demo/components/home/all-demands/create-update-offer.modal/create-update-offer-modal.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {OffersService} from '../../services/offers.service';
 import {DemandModel} from "../../../shared/models/demand.model";
+import { SharedService } from '../../../shared/service/shared.service';
 
 @Component({
     templateUrl: './create-update-offer-modal.component.html',
@@ -21,7 +22,7 @@ export class CreateUpdateOfferModalComponent implements OnInit {
     loading: boolean = false;
     submited: boolean = false;
 
-    constructor(private offersService: OffersService) {
+    constructor(private offersService: OffersService, private sharedService: SharedService) {
     }
 
     ngOnInit() {
@@ -64,128 +65,43 @@ export class CreateUpdateOfferModalComponent implements OnInit {
         }
     }
 
-    getBolean(boolean: any): any {
-        if (boolean == true)
-            return "Sim"
-        if (boolean == false)
-            return "Não"
+    Bolean(boolean: any): any {
+        return this.sharedService.getBolean(boolean);
     }
-
+    
     formatarData(dataString: string) {
-        const data = new Date(dataString);
-
-        const dia = String(data.getDate()).padStart(2, '0');
-        const mes = String(data.getMonth() + 1).padStart(2, '0');
-        const ano = data.getFullYear();
-
-        return `${dia}/${mes}/${ano}`;
+        return this.sharedService.getformatarData(dataString);
     }
-
-    getBedroomsNumber(bedroomsNumber: any): any {
-        if (bedroomsNumber == 'ONE')
-            return "Um quarto"
-        if (bedroomsNumber == 'TWO')
-            return "Dois quartos"
-        if (bedroomsNumber == 'THREE')
-            return "Três quartos"
-        if (bedroomsNumber == 'FOUR')
-            return "Quatro quartos"
-        if (bedroomsNumber == 'FIVE_OR_MORE')
-            return "Cinco ou mais quartos"
+    
+    BedroomsNumber(bedroomsNumber: any): any {
+        return this.sharedService.getBedroomsNumber(bedroomsNumber);
     }
-    getContractType(contractType: string): any {
-        if (contractType == 'RENT')
-            return 'Locação'
-        if (contractType == 'SALE')
-            return 'Venda'
-        if (contractType == 'SEASONAL')
-            return 'Temporada'
+    
+    ContractType(contractType: string): any {
+        return this.sharedService.getContractType(contractType);
     }
-
-    getPropertyType(propertyType: string): any {
-        if (propertyType == 'HOUSE')
-            return 'Casa'
-        if (propertyType == 'APARTMENT')
-            return 'Apartamento'
-        if (propertyType == 'COMMERCIAL')
-            return 'Ponto Comercial'
-        if (propertyType == 'ALLOTMENT')
-            return 'Loteamento'
-        if (propertyType == 'FARM')
-            return 'Sítio/Fazenda/Chácara'
+    
+    PropertyType(propertyType: string): any {
+        return this.sharedService.getPropertyType(propertyType);
     }
-
-    getLocation(address: any): any {
-        return address.city + ' - ' + address.state
+    
+    Location(address: any): any {
+        return this.sharedService.getLocation(address);
     }
-
-    getValue(propertyDemand: any): any {
-        if (propertyDemand.contractType == 'RENT')
-            return this.getValueForRent(propertyDemand.suggestedValueForRent);
-        if (propertyDemand.contractType == 'SALE')
-            return this.getValueForSale(propertyDemand.suggestedValueForSale);
-        if (propertyDemand.contractType == 'SEASONAL')
-            return this.getValueForSeasonal(propertyDemand.suggestedValueForSeasonal);
+    
+    Value(propertyDemand: any): any {
+        return this.sharedService.getValue(propertyDemand);
     }
-
-    getValueForRent(suggestedValueForRent: any): any {
-        if (suggestedValueForRent == 'R$1K')
-            return "R$ 1.000,00"
-        if (suggestedValueForRent == 'R$2K')
-            return "R$ 2.000,00"
-        if (suggestedValueForRent == 'R$3K')
-            return "R$ 3.000,00"
-        if (suggestedValueForRent == 'R$4K')
-            return "R$ 4.000,00"
-        if (suggestedValueForRent == 'R$5K')
-            return "R$ 5.000,00"
-        if (suggestedValueForRent == 'R$6K')
-            return "R$ 6.000,00"
-        if (suggestedValueForRent == 'R$10K')
-            return "R$ 10.000,00"
-        if (suggestedValueForRent == 'R$15K')
-            return "R$ 15.000,00"
+    
+    ValueForRent(suggestedValueForRent: any): any {
+        return this.sharedService.getValueForRent(suggestedValueForRent);
     }
-
-    getValueForSale(suggestedValueForSale: any): any {
-        if (suggestedValueForSale == 'R$400K')
-            return "R$ 400.000,00"
-        if (suggestedValueForSale == 'R$800K')
-            return "R$ 800.000,00"
-        if (suggestedValueForSale == 'R$1200K')
-            return "R$ 1.200.000,00"
-        if (suggestedValueForSale == 'R$1600K')
-            return "R$ 1.600.000,00"
-        if (suggestedValueForSale == 'R$2000K')
-            return "R$ 2.000.000,00"
-        if (suggestedValueForSale == 'R$2400K')
-            return "R$ 2.400.000,00"
-        if (suggestedValueForSale == 'R$5000K')
-            return "R$ 5.000.000,00"
-        if (suggestedValueForSale == 'R$10000K')
-            return "R$ 10.000.000,00"
-        if (suggestedValueForSale == 'R$15000K')
-            return "R$ 15.000.000,00"
-        if (suggestedValueForSale == 'R$20000K')
-            return "R$ 20.000.000,00"
+    
+    ValueForSale(suggestedValueForSale: any): any {
+        return this.sharedService.getValueForSale(suggestedValueForSale);
     }
-
-    getValueForSeasonal(suggestedValueForSeasonal: any): any {
-        if (suggestedValueForSeasonal == 'R$200')
-            return "R$ 200,00"
-        if (suggestedValueForSeasonal == 'R$400')
-            return "R$ 400,00"
-        if (suggestedValueForSeasonal == 'R$800')
-            return "R$ 800,00"
-        if (suggestedValueForSeasonal == 'R$1500')
-            return "R$ 1.500,00"
-        if (suggestedValueForSeasonal == 'R$2000')
-            return "R$ 2.000,00"
-        if (suggestedValueForSeasonal == 'R$2500')
-            return "R$ 2.500,00"
-        if (suggestedValueForSeasonal == 'R$3000')
-            return "R$ 3.000,00"
-        if (suggestedValueForSeasonal == 'R$5000')
-            return "R$ 5.000,00"
+    
+    ForSeasonal(suggestedValueForSeasonal: any): any {
+        return this.sharedService.getValueForSeasonal(suggestedValueForSeasonal);
     }
 }
--- src/app/demo/components/shared/service/shared.service.ts
+++ src/app/demo/components/shared/service/shared.service.ts
@@ -33,4 +33,128 @@ export class SharedService {
             'Content-Type': 'application/json'
         });
     }
+
+    getBolean(boolean: any): any {
+        if (boolean == true)
+            return "Sim"
+        if (boolean == false)
+            return "Não"
+    }
+
+    getformatarData(dataString: string) {
+        const data = new Date(dataString);
+        const dia = String(data.getDate()).padStart(2, '0');
+        const mes = String(data.getMonth() + 1).padStart(2, '0');
+        const ano = data.getFullYear();
+
+        return `${dia}/${mes}/${ano}`;
+    }
+
+    getBedroomsNumber(bedroomsNumber: any): any {
+        if (bedroomsNumber == 'ONE')
+            return "Um quarto"
+        if (bedroomsNumber == 'TWO')
+            return "Dois quartos"
+        if (bedroomsNumber == 'THREE')
+            return "Três quartos"
+        if (bedroomsNumber == 'FOUR')
+            return "Quatro quartos"
+        if (bedroomsNumber == 'FIVE_OR_MORE')
+            return "Cinco ou mais quartos"
+    }
+    getContractType(contractType: string): any {
+        if (contractType == 'RENT')
+            return 'Locação'
+        if (contractType == 'SALE')
+            return 'Venda'
+        if (contractType == 'SEASONAL')
+            return 'Temporada'
+    }
+
+    getPropertyType(propertyType: string): any {
+        if (propertyType == 'HOUSE')
+            return 'Casa'
+        if (propertyType == 'APARTMENT')
+            return 'Apartamento'
+        if (propertyType == 'COMMERCIAL')
+            return 'Ponto Comercial'
+        if (propertyType == 'ALLOTMENT')
+            return 'Loteamento'
+        if (propertyType == 'FARM')
+            return 'Sítio/Fazenda/Chácara'
+    }
+
+    getLocation(address: any): any {
+        return address.city + ' - ' + address.state
+    }
+
+    getValue(propertyDemand: any): any {
+        if (propertyDemand.contractType == 'RENT')
+            return this.getValueForRent(propertyDemand.suggestedValueForRent);
+        if (propertyDemand.contractType == 'SALE')
+            return this.getValueForSale(propertyDemand.suggestedValueForSale);
+        if (propertyDemand.contractType == 'SEASONAL')
+            return this.getValueForSeasonal(propertyDemand.suggestedValueForSeasonal);
+    }
+
+    getValueForRent(suggestedValueForRent: any): any {
+        if (suggestedValueForRent == 'R$1K')
+            return "R$ 1.000,00"
+        if (suggestedValueForRent == 'R$2K')
+            return "R$ 2.000,00"
+        if (suggestedValueForRent == 'R$3K')
+            return "R$ 3.000,00"
+        if (suggestedValueForRent == 'R$4K')
+            return "R$ 4.000,00"
+        if (suggestedValueForRent == 'R$5K')
+            return "R$ 5.000,00"
+        if (suggestedValueForRent == 'R$6K')
+            return "R$ 6.000,00"
+        if (suggestedValueForRent == 'R$10K')
+            return "R$ 10.000,00"
+        if (suggestedValueForRent == 'R$15K')
+            return "R$ 15.000,00"
+    }
+
+    getValueForSale(suggestedValueForSale: any): any {
+        if (suggestedValueForSale == 'R$400K')
+            return "R$ 400.000,00"
+        if (suggestedValueForSale == 'R$800K')
+            return "R$ 800.000,00"
+        if (suggestedValueForSale == 'R$1200K')
+            return "R$ 1.200.000,00"
+        if (suggestedValueForSale == 'R$1600K')
+            return "R$ 1.600.000,00"
+        if (suggestedValueForSale == 'R$2000K')
+            return "R$ 2.000.000,00"
+        if (suggestedValueForSale == 'R$2400K')
+            return "R$ 2.400.000,00"
+        if (suggestedValueForSale == 'R$5000K')
+            return "R$ 5.000.000,00"
+        if (suggestedValueForSale == 'R$10000K')
+            return "R$ 10.000.000,00"
+        if (suggestedValueForSale == 'R$15000K')
+            return "R$ 15.000.000,00"
+        if (suggestedValueForSale == 'R$20000K')
+            return "R$ 20.000.000,00"
+    }
+
+    getValueForSeasonal(suggestedValueForSeasonal: any): any {
+        if (suggestedValueForSeasonal == 'R$200')
+            return "R$ 200,00"
+        if (suggestedValueForSeasonal == 'R$400')
+            return "R$ 400,00"
+        if (suggestedValueForSeasonal == 'R$800')
+            return "R$ 800,00"
+        if (suggestedValueForSeasonal == 'R$1500')
+            return "R$ 1.500,00"
+        if (suggestedValueForSeasonal == 'R$2000')
+            return "R$ 2.000,00"
+        if (suggestedValueForSeasonal == 'R$2500')
+            return "R$ 2.500,00"
+        if (suggestedValueForSeasonal == 'R$3000')
+            return "R$ 3.000,00"
+        if (suggestedValueForSeasonal == 'R$5000')
+            return "R$ 5.000,00"
+    }
 }
