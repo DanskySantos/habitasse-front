b19371c7eeb84d6250e42e0e98f946c8c1965cbf
danSky <dansky123@hotmail.com>
2024-05-14 11:14:30 -0300

    adding key to aws

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,7 @@
 export const environment = {
     apiUrl: 'http://localhost:8080/api/v1/',
     clientId: 'qwsdfgsdfgsdetersadf',
-    clientSecret: '@20242opasijopdjoipfasdjpio3%'
+    clientSecret: '@20242opasijopdjoipfasdjpio3%',
+    awsAccessKey: 'AKIAQ3EGVHXCMI5HLS7M',
+    awsSecretKey: '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',
 }
