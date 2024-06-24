cca868fc2cca528552ab075607005a58d9716ac6
DanskySantos <dansky123@hotmail.com>
2024-06-24 17:11:31 -0300

    creating warn modal and excluding unused data

--- src/app/demo/components/home/all-demands/all-demands.component.ts
+++ src/app/demo/components/home/all-demands/all-demands.component.ts
@@ -29,6 +29,7 @@ import {Router} from "@angular/router";
 export class AllDemandsComponent implements OnInit {
 
     visible: boolean = false;
+    isWarnVisible: boolean = true;
     demands: any;
     filterForm!: FormGroup;
     totalElements!: number;
